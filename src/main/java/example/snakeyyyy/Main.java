5f13640d7a2a820d5485cd1807cdbe4df4b4d0da
psyjm21 <psyjm21@exmail.nottingham.ac.uk>
2022-11-28 18:50:42 +0000

    working code, no changes

---
+++ src/main/java/example/snakeyyyy/Main.java
@@ -0,0 +1,7 @@
+package example.snakeyyyy;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/example/snakeyyyy/movable.java
@@ -0,0 +1,14 @@
+package example.snakeyyyy;
+
+/**
+ * 
+ * @Project Snakee
+ * @Description Fyrsta viðmótið
+ * @Author Sigurður Sigurðardóttir
+ * @version Ekki viss
+ */ 
+
+public interface movable
+{
+	void move();
+}
