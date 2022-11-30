e7dec5d6121c1b81c9eda9c2fc2b5578b607d4df
psyjm21 <psyjm21@exmail.nottingham.ac.uk>
2022-11-30 20:47:07 +0000

    start menu with banner and buttons

--- src/main/java/example/snakeyyyy/StartMenu.java
+++ src/main/java/example/snakeyyyy/StartMenu.java
@@ -1,22 +1,41 @@
 package example.snakeyyyy;
 import java.awt.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 public class StartMenu extends JFrame {
     StartGame StartGame;
     Image label = ImageUtil.images.get("start-menu-banner");
+    Icon icon = new ImageIcon((Image) label);
     public StartMenu() {
-        setLayout(new BorderLayout());
-        add(new JLabel("Snakeyyyy", (JLabel.CENTER)), BorderLayout.CENTER);
-        JButton StartButton = new JButton("Start:");
-        JButton quitButton = new JButton("Exit");
+
+        //defining panels
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+
+        JLabel sticker = new JLabel(icon);
+
+        BoxLayout boxlayout = new BoxLayout(panel1, BoxLayout.Y_AXIS);
+
+        panel1.setLayout(boxlayout);
+        panel1.setBorder(new EmptyBorder(new Insets(150, 200, 150, 200)));
+
+        JButton StartButton = new JButton("Start: --------");
+        JButton quitButton = new JButton("Exit--------");
         quitButton.addActionListener(e -> System.exit(0));
         StartButton.addActionListener(e -> StartGame.startGame());
-        add(StartButton, BorderLayout.EAST);
-        add(quitButton, BorderLayout.SOUTH);
+
+        panel1.add(sticker);
+
+        panel2.add(StartButton);
+        panel2.add(quitButton);
+
+        add(panel1);
+        add(panel2);
+        pack();
+        setVisible(true);
         setSize(600, 600);
         setLocationRelativeTo(null);
         setVisible(true);
