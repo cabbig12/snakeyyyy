d469138a553f2df175a9a1b1d8defd902c89b099
psyjm21 <psyjm21@exmail.nottingham.ac.uk>
2022-12-01 20:13:10 +0000

    StartMenu refactored into different methods

--- src/main/java/example/snakeyyyy/StartMenu.java
+++ src/main/java/example/snakeyyyy/StartMenu.java
@@ -11,44 +11,50 @@ public class StartMenu extends JFrame {
     StartGame StartGame;
     Image label = ImageUtil.images.get("start-menu-banner");
     Icon icon = new ImageIcon((Image) label);
+
+    //defining panels
+    JPanel panel1 = new JPanel();
+    JPanel panel2 = new JPanel();
+    JPanel panel3 = new JPanel();
+    JPanel panel4 = new JPanel();
+
+    //Defining enterName label
+    JLabel EnterName = new JLabel("Enter Name: ");
+
+    //Defining highscore labels
+    JLabel HighScore1 = new JLabel("This is a label1");
+    JLabel HighScore2 = new JLabel("This is a label2");
+    JLabel HighScore3 = new JLabel("This is a label3");
+
+    //defining snakeyyyy logo
+    JLabel sticker = new JLabel(icon);
+
+    //defining input text box
+    JTextField textField = new JTextField(5);
+
+    //making quit, submit and start button
+    JButton submitButton = new JButton("Submit");
+    JButton quitButton = new JButton("Exit");
+    JButton StartButton = new JButton("Start");
     public StartMenu() {
 
-        //defining panels
-        JPanel panel1 = new JPanel();
-        JPanel panel2 = new JPanel();
-        JPanel panel3 = new JPanel();
-        JPanel panel4 = new JPanel();
+        StyleStartMenu();
+        AddStartMenuObjects();
+        StartMenuObjectFunctionality();
 
+    }
+
+    public void StyleStartMenu(){
         // defining background colors
         panel1.setBackground(Color.green);
         panel2.setBackground(Color.green);
         panel3.setBackground(Color.green);
         panel4.setBackground(Color.green);
 
-        //Defining enterName label
-        JLabel EnterName = new JLabel("Enter Name: ");
-
-        //Defining highscore labels
-        JLabel HighScore1 = new JLabel("This is a label1");
-        JLabel HighScore2 = new JLabel("This is a label2");
-        JLabel HighScore3 = new JLabel("This is a label3");
-
-        //defining snakeyyyy logo
-        JLabel sticker = new JLabel(icon);
-        
-        //defining input text box
-        JTextField textField = new JTextField(5);
-
-        //making quit, submit and start button
-        JButton submitButton = new JButton("Submit");
-        JButton quitButton = new JButton("Exit");
-        JButton StartButton = new JButton("Start");
-
         //styling highscore labels
         HighScore1.setFont(new Font("Verdana",1,20));
         HighScore2.setFont(new Font("Verdana",1,20));
         HighScore3.setFont(new Font("Verdana",1,20));
-
 
         //setting jpanels to boxlayout
         BoxLayout layout1 = new BoxLayout(panel1, BoxLayout.X_AXIS);
@@ -66,11 +72,21 @@ public class StartMenu extends JFrame {
         submitButton.setMaximumSize( new Dimension(     80, 30 ) );
         textField.setMaximumSize(new Dimension( 150, 30 ));
 
+        //positioning frame nicely
+        panel1.setBorder(BorderFactory.createEmptyBorder(10, 193, 10, 10));
+        panel2.setBorder(BorderFactory.createEmptyBorder(2, 150, 10, 50));
+        panel3.setBorder(BorderFactory.createEmptyBorder(70, 120, 100, 100));
+        panel4.setBorder(BorderFactory.createTitledBorder("HighScore"));
 
-        //adding functionality to start and quit button
-        quitButton.addActionListener(e -> System.exit(0));
-        StartButton.addActionListener(e -> StartGame.startGame());
-        StartButton.addActionListener(e -> dispose());
+        // screen settings
+        pack();
+        setVisible(true);
+        setSize(600, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public void AddStartMenuObjects(){
 
 
         //adding snakeyyyy label to the 1st panel
@@ -97,14 +113,19 @@ public class StartMenu extends JFrame {
         add(panel2);
         add(panel3);
         add(panel4);
+    }
 
-        //positioning frame nicely
-        panel1.setBorder(BorderFactory.createEmptyBorder(10, 193, 10, 10));
-        panel2.setBorder(BorderFactory.createEmptyBorder(2, 150, 10, 50));
-        panel3.setBorder(BorderFactory.createEmptyBorder(70, 120, 100, 100));
-        panel4.setBorder(BorderFactory.createTitledBorder("HighScore"));
+    public void StartMenuObjectFunctionality(){
 
+        //adding functionality to start and quit button
+        quitButton.addActionListener(e -> System.exit(0));
+        StartButton.addActionListener(e -> StartGame.startGame());
+        StartButton.addActionListener(e -> dispose());
+
+        // Setting start button to be invisible
         StartButton.setVisible(false);
+
+        // making start button visible once submit button gets pressed
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -113,11 +134,7 @@ public class StartMenu extends JFrame {
             }
         });
 
-        pack();
-        setVisible(true);
-        setSize(600, 600);
-        setLocationRelativeTo(null);
-        setVisible(true);
-
     }
+
+
 }
